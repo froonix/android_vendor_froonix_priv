861c091a92b644378209c6474a700c853a5d8db3
Christian <killerbees19@users.noreply.github.com>
2021-03-29 20:35:10 +0200

    Update keys.mk

--- keys/keys.mk
+++ keys/keys.mk
@@ -1,11 +1,11 @@
 ifneq ($(FROONIX_PRIV_BUILD),)
     ifneq ($(FROONIX_PRIV_BUILD),false)
 
+        ## Allow downgrading -- it's an OTA server decision!
+        #PRODUCT_PROPERTY_OVERRIDES += lineage.updater.allow_downgrading=true
+
         # Change OTA server URL for our builds.
         PRODUCT_PROPERTY_OVERRIDES += lineage.updater.uri=https://ota.fnx.li/los/$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)/$(shell tr '[:upper:]' '[:lower:]' <<< $(FROONIX_PRIV_BUILD_ID))-$(shell tr '[:upper:]' '[:lower:]' <<< $(LINEAGE_BUILDTYPE))/{device}/{type}/{incr}
-
-        # Allow downgrading -- it's an OTA server decision!
-        PRODUCT_PROPERTY_OVERRIDES += lineage.updater.allow_downgrading=true
 
         # Modify version string...
         ifneq ($(FROONIX_PRIV_BUILD_ID),)
